9103f8e2d076d816083dce8d98dc5aaaf1f22844
bralash <emmanuelasaber@gmail.com>
2024-09-17 15:51:48 +0000

    Adds Form Array

---
+++ src/app/form-array/README.md
@@ -0,0 +1,2 @@
+# Form Array
+This component demonstrates how to use `FormArray` to create a dynamic form with the ability to add and remove form controls.
--- src/app/form-array/form-array.component.ts
+++ src/app/form-array/form-array.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  NgModel,
+  Validators,
+} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form-array',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './form-array.component.html',
-  styleUrl: './form-array.component.scss'
+  styleUrl: './form-array.component.scss',
 })
-export class FormArrayComponent {
+export class FormArrayComponent implements OnInit {
+  form!: FormGroup;
+  submittedItems: string[] = [];
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      items: this.fb.array([]),
+    });
+    this.addItem();
+  }
+
+  addItem() {
+    this.itemsFormArray.push(this.fb.control('', Validators.required));
+  }
+
+  get itemsFormArray() {
+    return this.form.get('items') as FormArray;
+  }
+
+  removeItem(index: number) {
+    this.itemsFormArray.removeAt(index);
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.submittedItems = this.itemsFormArray.value;
+    }
+  }
 }
