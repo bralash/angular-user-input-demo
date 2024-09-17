dbc85af3c9247d7dbff5b3a366d3a9267a248f40
bralash <emmanuelasaber@gmail.com>
2024-09-17 12:17:55 +0000

    Adds implementation for Event Binding

---
+++ src/app/event-binding/README.md
@@ -0,0 +1,2 @@
+#Event Binding
+This component demonstrates event binding for click and keyup events, showing how to access the `$event` object.
--- src/app/event-binding/event-binding.component.ts
+++ src/app/event-binding/event-binding.component.ts
@@ -8,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './event-binding.component.scss'
 })
 export class EventBindingComponent {
+  lastEvent: string = '';
 
+  onClick(event: MouseEvent) {
+    this.lastEvent = `Clicked at: (${event.clientX}, ${event.clientY})`;
+  }
+
+  onKeyUp(event: KeyboardEvent) {
+    this.lastEvent = `Key pressed: ${event.key}`
+  }
 }
