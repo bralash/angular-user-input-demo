34b32c9a0388e6bda4601b2866df43202c903159
bralash <emmanuelasaber@gmail.com>
2024-09-17 12:38:07 +0000

    Implements view content child

---
+++ src/app/view-content-child/README.md
@@ -0,0 +1,2 @@
+# View Content Child
+This component demonstrates the use of `@ViewChild` to access an element within the component's template, and `@ContentChild` to access content projected into the component.
--- src/app/view-content-child/view-content-child.component.ts
+++ src/app/view-content-child/view-content-child.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ContentChild, ElementRef, ViewChild, AfterViewInit, AfterContentInit } from '@angular/core';
 
 @Component({
   selector: 'app-view-content-child',
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './view-content-child.component.scss'
 })
 export class ViewContentChildComponent {
+  @ViewChild('viewChildInput') viewChildInput!: ElementRef;
+  @ContentChild('contentChildInput') contentChildInput!: ElementRef;
 
+  ngAfterViewInit() {
+    console.log('ViewChild:', this.viewChildInput);
+  }
+
+  ngAfterContentInit() {
+    console.log('ContentChild:', this.contentChildInput);
+  }
+
+  focusViewChildInput() {
+    this.viewChildInput.nativeElement.focus();
+  }
+
+  focusContentChildInput() {
+    this.contentChildInput.nativeElement.focus();
+  }
 }
